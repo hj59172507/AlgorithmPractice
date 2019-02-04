35736e73d480b81a37dd1bab922f9d953799a537
hj59172507 <huaj59172507@gmail.com>
2019-02-04 16:25:52 -0500

    initial setup for greedy approach schedueling problem and minimum spanning tree(MST) problem

---
+++ Greedy/PrimsMSTUsingHeap/PrimsMSTUsingHeap.py
@@ -0,0 +1,13 @@
+'''
+The edges.txt describes an undirected graph with integer edge costs. It has the format
+
+[number_of_nodes] [number_of_edges]
+
+[one_node_of_edge_1] [other_node_of_edge_1] [edge_1_cost]
+
+[one_node_of_edge_2] [other_node_of_edge_2] [edge_2_cost]
+
+For example, the third line of the file is "2 3 -8874", indicating that there is an edge connecting vertex #2 and vertex #3 that has cost -8874.
+
+edge costs are not necessary positive, nor distinct.
+'''
---
+++ Greedy/ScheduleJobGreedyWay/ScheduleJobGreedyWay.py
@@ -0,0 +1,13 @@
+'''
+The jobs.txt file describes a set of jobs with positive and integral weights and lengths. It has the format
+
+[number_of_jobs]
+
+[job_1_weight] [job_1_length]
+
+[job_2_weight] [job_2_length]
+
+For example, the third line of the file is "74 59", indicating that the second job has weight 74 and length 59.
+
+You should NOT assume that edge weights or lengths are distinct.
+'''
