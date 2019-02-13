ee28c5ecdc72e45e1cf7ffe71f000073137c9b68
hj59172507 <huaj59172507@gmail.com>
2019-02-12 19:46:21 -0500

    finish minWaitTime problem using min heap, time complexity is O(nlog(n))

--- bruteForce/minWaitTime/minWaitTime.py
+++ bruteForce/minWaitTime/minWaitTime.py
@@ -10,3 +10,134 @@ time required to cook that pizza.
 
 Display the integer part of the minimum average waiting time.
 '''
+
+class MinHeap:
+    '''MinHeap class for numeric list'''
+    def __init__(self):
+        self.heap = []
+    
+    #get parent index from childIndex
+    def parent(self, childIndex):
+        if(childIndex == 0):
+            return 0
+        return (childIndex+1)//2-1
+
+    #get left child index from parentIndex
+    def left(self, parentIndex):
+        return (parentIndex*2)+1
+
+    #get right child index from parentIndex
+    def right(self, parentIndex):
+        return (parentIndex*2)+2
+
+    #move element at index i down to proper place so heap properity is maintained
+    def heapifyDown(self, i):
+        left, right, smallest = self.left(i), self.right(i), i
+        if(left < len(self.heap) and self.heap[left][1] < self.heap[i][1]):
+            smallest = left
+        if(right < len(self.heap) and self.heap[right][1] < self.heap[smallest][1]):
+            smallest = right
+        if(smallest != i):
+            self.swap(smallest, i)
+            self.heapifyDown(smallest)
+
+    #move element at index i up to proper place so heap properity is maintained
+    def heapifyUp(self, i):
+        parent = self.parent(i)
+        if(i != parent and self.heap[i][1] < self.heap[parent][1]):
+            self.swap(i, parent)
+            self.heapifyUp(parent)
+
+    #swap values of elements at heap index x and y
+    def swap(self, x, y):
+        self.heap[x], self.heap[y] = self.heap[y], self.heap[x]
+
+    #heapity an unordered array, assuming arr is integer array
+    def heapifyArray(self, arr):
+        self.heap = arr
+        i = len(self.heap)//2
+        while(i > 0):
+            self.heapifyDown(i)
+            i -= 1
+
+    #insert an element to heap, and maintain heap property
+    def insert(self, element):
+        self.heap.append(element)
+        self.heapifyUp(len(self.heap)-1)
+
+    #return the minimum element in the heap without removing it
+    def peek(self):
+        if(len(self.heap) > 0):
+            return self.heap[0]
+        else:
+            return None
+
+    #delete an element in index i from heap
+    def deleteAt(self, i):
+        if(i != len(self.heap)-1):
+            self.swap(i, len(self.heap)-1)
+            del self.heap[len(self.heap)-1]
+            self.heapifyDown(i)
+        else:
+            del self.heap[i]
+
+    #delete an element in heap, return  -1 if not found
+    def deleteEle(self, element):
+        elementIndex = self.heap.index(element)
+        self.deleteAt(elementIndex)
+
+    #return the minimum element in the heap and remove it
+    def pop(self):
+        if(len(self.heap) > 0):
+            result = self.heap[0]
+            self.deleteAt(0)
+            return result
+        else:
+            return None
+
+    def size(self):
+        return len(self.heap)
+
+#move customers from customers to heap if they arrive before or on arriveTime
+def addArrivedCustomer(nextServeCandidates, customers, arriveTime):
+	while(len(customers) > 0 and customers[0][0] <= arriveTime):
+		nextServeCandidates.insert(customers[0])
+		del customers[0]
+
+def minimumAverage(customers):
+	#sort by arrive time
+	customers = sorted(customers, key = lambda x:x[0])
+	firstArriveTime, customersCount, servedCustomerCount = customers[0][0], len(customers), 0
+	nextServeCandidates = MinHeap()
+	addArrivedCustomer(nextServeCandidates, customers, firstArriveTime)
+
+	totalWaitTime = 0
+	currentTime = firstArriveTime
+
+	#loop to handle one customer at a time
+	while(servedCustomerCount != customersCount):
+		#there is at least one customer in line, pop the one with least pizza cooking time
+		if(nextServeCandidates.size()>0):
+			customer = nextServeCandidates.pop()
+			#cook the order of above customer
+			currentTime += customer[1]
+			#add his/her wait time to totalWaitTime, which is define by currentTime - time he/she arrived
+			totalWaitTime += currentTime - customer[0]
+			servedCustomerCount += 1
+			#add more arrived customer according to currentTime
+			if(len(customers) > 0):
+				addArrivedCustomer(nextServeCandidates, customers, currentTime)
+		#skip to the time next customer arrives if there are more customers in future
+		elif(len(customers) > 0):
+			currentTime = customers[0][0]
+			addArrivedCustomer(nextServeCandidates, customers, currentTime)
+	return int(totalWaitTime/customersCount)
+
+
+customers = []
+with open('input.txt') as file:
+	for data in file.readlines():
+		customers.append([int(x) for x in data.split()])
+
+
+print(minimumAverage(customers))
