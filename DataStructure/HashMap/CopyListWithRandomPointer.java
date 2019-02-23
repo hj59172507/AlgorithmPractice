7e49bc5e9bae9f6fbe296867638de9c3b532642d
hj59172507 <huaj59172507@gmail.com>
2019-02-23 01:23:06 -0500

    add another solution, now the deep copy use a third link list instead of hashMap, time and memory still O(n) but constant on both is smaller

--- DataStructure/HashMap/CopyListWithRandomPointer.java
+++ DataStructure/HashMap/CopyListWithRandomPointer.java
@@ -57,3 +57,54 @@ class Node {
         return newNodeHead;
     }
 
+//solution 2 without Hashmap, even less memory require, time complexity is same
+    public Node copyRandomList(Node head) {
+        //base case
+        if(head == null)
+            return null;
+               
+        Node ori = head, oriTemp = ori, newNodeHead = new Node(ori.val, null, ori), newNodeTemp = newNodeHead;
+        Node oriFixHead = new Node(0, null, head), oriFixTemp = oriFixHead, tempFix = null;
+        ori = ori.next;
+        oriTemp.next = newNodeHead;
+        
+        //loop to copy the list of node, set new node random to respective ori node, and set ori.next to new node
+        while(ori != null){
+            Node tempNode = new Node(ori.val, null, ori);
+            oriTemp = ori;
+            ori = ori.next;
+            oriTemp.next = tempNode;
+            newNodeTemp.next = tempNode;
+            newNodeTemp = newNodeTemp.next;
+        }
+        
+        newNodeTemp = newNodeHead;
+
+        //update random node of new list, save the next pointer of original list
+        while(newNodeTemp != null){
+            ori = newNodeTemp.random;
+            if(ori.random != null)
+                newNodeTemp.random = ori.random.next;
+            else
+                newNodeTemp.random = null;
+            if(newNodeTemp.next != null){
+                tempFix = new Node(0, null, newNodeTemp.next.random);
+                oriFixTemp.next = tempFix;
+            }
+            newNodeTemp = newNodeTemp.next;
+        }
+        
+        //fix original list
+        oriFixTemp = oriFixHead;
+        ori = head;
+        while(oriFixTemp != null){
+            if(oriFixTemp.next != null)
+                ori.next = oriFixTemp.next.random;
+            else
+                ori.next = null;
+            ori = ori.next;
+            oriFixTemp = oriFixTemp.next;
+        }
+        
+        return newNodeHead;
+    }
