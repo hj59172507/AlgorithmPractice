5818845e02823e54ed4682c9b475f8efe90d5dfa
hj59172507 <huaj59172507@gmail.com>
2019-02-23 16:47:46 -0500

    implement finding number of islands using DFS

---
+++ Graph/numberOfIslands.java
@@ -0,0 +1,69 @@
+/*
+Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. 
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. 
+You may assume all four edges of the grid are all surrounded by water.
+*/
+
+//Solution 1 using DFS
+    public int numIslands(char[][] grid) {
+        //base case 
+        if(grid.length == 0 || grid[0].length == 0)
+            return 0;
+        int row = grid.length;
+        int col = grid[0].length;
+        int islands = 0;
+        //set up 2d boolean grid to store position visited
+        boolean [][] visited = new boolean[row][col];
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                //explore the position if it is not visited and not water
+                if(!visited[i][j] && grid[i][j] == '1'){
+                    islands++;
+                    visit(visited, grid, i, j);
+                }
+            }
+        }
+        return islands;
+    }
+    
+    //explore all direction of given point by calling visit
+    public void explore(boolean[][] visited, char[][] grid, int x, int y){
+        int maxX = visited.length;
+        int maxY = visited[0].length;
+        //visit up if new position is within bound and not visited
+        if(checkLowerBound(y-1, 0) && !visited[x][y-1]){
+            visit(visited, grid, x, y-1);
+        }
+        //visit down recursively if new position is within bound and not visited
+        if(checkUpperBound(y+1, maxY) && !visited[x][y+1]){
+            visit(visited, grid, x, y+1);
+        }
+        //visit left recursively if new position is within bound and not visited
+        if(checkLowerBound(x-1, 0) && !visited[x-1][y]){
+            visit(visited, grid, x-1, y);
+        }
+        //visit right recursively if new position is within bound and not visited
+        if(checkUpperBound(x+1, maxX) && !visited[x+1][y]){
+            visit(visited, grid, x+1, y);
+        }
+        
+    }
+    
+    //visit this position, and call explore to search other directions of given position
+    public void visit(boolean[][] visited, char[][] grid, int x, int y){
+        visited[x][y] = true;
+        //continue to explore if this position is island
+        if(grid[x][y] == '0')
+            return;
+        explore(visited, grid, x, y);
+    }
+    
+    //check upper bound 
+    public boolean checkUpperBound(int d, int maxD){
+        return d < maxD;
+    }
+    
+    //check lower bound
+    public boolean checkLowerBound(int d, int minD){
+        return d >= 0 ;
+    }
