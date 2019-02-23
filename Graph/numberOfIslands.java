59836456d2085cba12168f0d866891db1d5a49e2
hj59172507 <huaj59172507@gmail.com>
2019-02-23 17:04:32 -0500

    add BFS solution, both DFS and BFS have time complexity of O(MN) where M and N are dimention of the grid to scan

--- Graph/numberOfIslands.java
+++ Graph/numberOfIslands.java
@@ -67,3 +67,52 @@ You may assume all four edges of the grid are all surrounded by water.
     public boolean checkLowerBound(int d, int minD){
         return d >= 0 ;
     }
+
+//solution 2: using DFS
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+          return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+
+        for (int r = 0; r < nr; ++r) {
+          for (int c = 0; c < nc; ++c) {
+            if (grid[r][c] == '1') {
+              ++num_islands;
+              grid[r][c] = '0'; // mark as visited
+              Queue<Integer> neighbors = new LinkedList<>();
+              neighbors.add(r * nc + c);
+              while (!neighbors.isEmpty()) {
+                int id = neighbors.remove();
+                int row = id / nc;
+                int col = id % nc;
+                //visit up
+                if (row - 1 >= 0 && grid[row-1][col] == '1') {
+                  neighbors.add((row-1) * nc + col);
+                  grid[row-1][col] = '0';
+                }
+                //visit down
+                if (row + 1 < nr && grid[row+1][col] == '1') {
+                  neighbors.add((row+1) * nc + col);
+                  grid[row+1][col] = '0';
+                }
+                //visit left
+                if (col - 1 >= 0 && grid[row][col-1] == '1') {
+                  neighbors.add(row * nc + col-1);
+                  grid[row][col-1] = '0';
+                }
+                //visit right
+                if (col + 1 < nc && grid[row][col+1] == '1') {
+                  neighbors.add(row * nc + col+1);
+                  grid[row][col+1] = '0';
+                }
+              }
+            }
+          }
+        }
+
+        return num_islands;
+    }
