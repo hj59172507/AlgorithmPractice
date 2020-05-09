893686c0c2c685d62c513b59904e9266af438e8d
huajian lin <huaj59172507@gmail.com>
2020-05-08 20:31:40 -0400

    some more simple practice

---
+++ LeetCodePractice/SimpleWarmUp/SmallerNumbersThanCurrentNumber.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.SimpleWarmUp
+{
+    class SmallerNumbersThanCurrentNumber
+    {
+        //static void Main()
+        static void Main1365()
+        {
+            int[] nums = new int[] { 8, 1, 2, 2, 3 };
+            printArray(smallerNumbersThanCurrent(nums));
+            Console.In.ReadLine();
+        }
+        public static int[] smallerNumbersThanCurrent(int[] nums)
+        {
+            int[] result = new int[nums.Length];
+            for(int i = 0; i < nums.Length; i++)
+            {                
+                for(int j = i+1; j < nums.Length; j++)
+                {
+                    if(nums[j] < nums[i])
+                    {
+                        result[i]++;
+                    }
+                    if (nums[j] > nums[i])
+                    {
+                        result[j]++;
+                    }
+                }
+             
+            }
+            return result;
+            /*
+            Bucket solution base on restriction
+            int[] bucket = new int[102];
+            for(int i=0; i<nums.length; i++)
+                bucket[nums[i]+1]++;
+
+            for(int i=1; i<102; i++) 
+                bucket[i] += bucket[i-1];
+        
+            for(int i=0; i<nums.length; i++)
+                nums[i] = bucket[nums[i]];
+        
+            return nums;
+             * */
+        }
+
+        public static void printArray(IEnumerable arr)
+        {
+            foreach(var a in arr)
+            {
+                Console.Out.WriteLine(a);
+            }
+        }
+    }
+}
---
+++ LeetCodePractice/SimpleWarmUp/SubtractProdcuAndSumOfInt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.SimpleWarmUp
+{
+    class SubtractProdcuAndSumOfInt
+    {
+        //static void Main()
+        static void Main1281()
+        {
+            int n = 234;
+            Console.Out.WriteLine(SubtractProductAndSum(n));
+            Console.In.ReadLine();
+        }
+        public static int SubtractProductAndSum(int n)
+        {
+            int product = 1;
+            int sum = 0;
+            while(n != 0)
+            {
+                int rem = n % 10;
+                product *= rem;
+                sum += rem;
+                n /= 10;
+            }
+            return product - sum;
+        }
+    }
+}
