049bac1ab4814a1dc11c933225ee11a64129a3eb
huajian lin <huaj59172507@gmail.com>
2020-05-09 12:39:59 -0400

    some tree and map practice

---
+++ LeetCodePractice/DataStructure/HashMap/GroupPeopleBySize.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+There are n people whose IDs go from 0 to n - 1 and each person belongs exactly to one group.Given the array groupSizes of length n telling the group size each person belongs to, return the groups there are and the people's IDs each group includes.
+
+You can return any solution in any order and the same applies for IDs.Also, it is guaranteed that there exists at least one solution. 
+
+
+
+
+Example 1:
+
+Input: groupSizes = [3, 3, 3, 3, 3, 1, 3]
+Output: [[5], [0,1,2], [3,4,6]]
+Explanation: 
+Other possible solutions are[[2,1,6],[5],[0,4,3]] and[[5],[0,6,2],[4,3,1]].
+Example 2:
+
+Input: groupSizes = [2,1,3,3,3,2]
+Output: [[1],[0,5],[2,3,4]]
+ 
+
+Constraints:
+
+groupSizes.length == n
+1 <= n <= 500
+1 <= groupSizes[i] <= n
+*/
+
+namespace LeetCodePractice.DataStructure.HashMap
+{
+    class GroupPeopleBySize
+    {
+
+        //static void Main()
+        static void Main1282()
+        {
+            int[] input =  new int[] { 3, 3, 3, 3, 3, 1, 3 };
+            Console.Out.WriteLine(GroupThePeople(input));
+            Console.In.ReadLine();
+        }
+
+        public static IList<IList<int>> GroupThePeople(int[] groupSizes)
+        {
+            IList<IList<int>> res = new List<IList<int>> { };
+            Dictionary<int, List<int>> map = new Dictionary<int, List<int>>();
+
+            for (int i = 0; i < groupSizes.Length; i++)
+            {
+                int groupSize = groupSizes[i];
+                if (map.ContainsKey(groupSize))
+                {
+                    if(map[groupSize][0] == map[groupSize].Count)
+                    {
+                        map[groupSize][0] = i;
+                        res.Add(map[groupSize]);
+                        map.Remove(groupSize);
+                    }
+                    else
+                    {
+                        map[groupSize].Add(i);
+                    }
+                }
+                else if(groupSize == 1)
+                {
+                    res.Add(new List<int> { i });
+                }
+                else
+                {
+                    map.Add(groupSize, new List<int> { groupSize, i });
+                }
+            }
+            return res;
+        }
+    }
+
+}
---
+++ LeetCodePractice/DataStructure/Tree/DesspestLeavesSumInOrder.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Activation;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.DataStructure
+{
+    /*
+    Given a binary tree, return the sum of values of its deepest leaves.
+
+    Example 1:
+
+    Input: root = [1,2,3,4,5,null,6,7,null,null,null,null,8]
+    Output: 15
+ 
+    Constraints:
+
+    The number of nodes in the tree is between 1 and 10^4.
+    The value of nodes is between 1 and 100.
+     */
+    class DeepestLeavesSumInOrder
+    {
+        //static void Main()
+        static void Main1302()
+        {
+            TreeNode left = new TreeNode(2, new TreeNode(4, new TreeNode(7)), new TreeNode(5));
+            TreeNode right = new TreeNode(3, null, new TreeNode(6, null, new TreeNode(8)));
+            TreeNode root = new TreeNode(1, left, right);
+            Console.Out.WriteLine(DeepestLeavesSum(root));
+            Console.In.ReadLine();
+        }
+        
+        public static int DeepestLeavesSum(TreeNode root)
+        {
+            List<int> levelSum = new List<int> {0};
+            DLSHelper(root, levelSum, 0);
+            int deepestLevel = levelSum[0];
+            int sum = 0;
+            for(int i=0; i<= levelSum.Count/2-1; i++)
+            {
+                int levelIndex = i * 2 + 1;
+                if(levelSum[levelIndex] == deepestLevel)
+                {
+                    sum += levelSum[i*2+2];
+                }
+            }
+            return sum;
+        }
+
+        public static void DLSHelper(TreeNode root, List<int> levelSum, int currentLevel)
+        {
+     
+            if (root.left != null)
+            {
+                DLSHelper(root.left, levelSum, currentLevel + 1);
+            }
+            if(root.right != null)
+            {
+                DLSHelper(root.right, levelSum, currentLevel + 1);
+            }
+            if(currentLevel > levelSum[0])
+            {
+                levelSum[0] = currentLevel;
+            }
+            levelSum.Add(currentLevel);
+            levelSum.Add(root.val);
+        }
+    }
+
+    public class TreeNode
+    {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+     public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+            }
+    }
+}
