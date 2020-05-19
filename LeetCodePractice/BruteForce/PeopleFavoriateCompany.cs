3847f21155979bc907e16dae98dc5d4e987449be
huajian lin <huaj59172507@gmail.com>
2020-05-19 16:13:35 -0400

    subset problem with bit operation solution

--- LeetCodePractice/BruteForce/PeopleFavoriateCompany.cs
+++ LeetCodePractice/BruteForce/PeopleFavoriateCompany.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 /*
@@ -38,6 +39,11 @@ All strings consist of lowercase English letters only.
 
 Sol1 Brute Force:
 Turn all favorite companies into set, and use subset comparison method.
+
+Sol2 Bit operation:
+1. Assign a number to each string(since string is unique, there will not be a duplicate)
+2. Create a bit map for each favorite company lise
+3. Use bit & operation to compare, if a & b == a, then b is a subset of a
  */
 namespace LeetCodePractice.BruteForce
 {
@@ -58,23 +64,45 @@ namespace LeetCodePractice.BruteForce
         public static IList<int> PeopleIndexes(IList<IList<string>> favoriteCompanies)
         {
             List<int> res = new List<int>();
-            List<HashSet<String>> fcs = new List<HashSet<string>>();            
-            foreach(IList<string> fc in favoriteCompanies)
+            Dictionary<string, int> companyId = new Dictionary<string, int>();
+            int id = 0;
+            foreach (IList<string> fc in favoriteCompanies)
             {
-                fcs.Add(new HashSet<string>(fc));
+                foreach(string s in fc)
+                {
+                    if (!companyId.ContainsKey(s))
+                    {
+                        companyId[s] = id++;
+                    }
+                }
             }
-            
-            for(int i = 0; i < fcs.Count; i++)
+
+            List<BitArray> fcBitArr = new List<BitArray>();
+            foreach (IList<string> fc in favoriteCompanies)
+            {
+                BitArray temp = new BitArray(id);
+                foreach (string s in fc)
+                {
+                    temp[companyId[s]] = true;                    
+                }
+                fcBitArr.Add(temp);
+            }
+
+            for (int i = 0; i < favoriteCompanies.Count; i++)
             {
                 bool subset = false;
-                for (int j = 0; j < fcs.Count; j++)
+                for (int j = 0; j < favoriteCompanies.Count; j++)
                 {
                     if (i != j)
                     {
-                        subset = fcs[i].IsSubsetOf(fcs[j]);
+                        BitArray temp = new BitArray(fcBitArr[i]);
+                        subset = BitArrayCompare(fcBitArr[i].And(fcBitArr[j]), temp);
+                        fcBitArr[i] = temp;
                     }
                     if (subset)
+                    {
                         break;
+                    }
                 }
                 if (!subset)
                 {
@@ -84,6 +112,50 @@ namespace LeetCodePractice.BruteForce
 
             return res;
         }
+
+        //This compare method cause time limit to exceed if number of distint string is large enough
+        public static bool BitArrayCompare(BitArray a, BitArray b)
+        {
+            bool identical = true;
+            //assume length is same
+            for(int i = 0; i < a.Length; i++)
+            {
+                if(a[i] != b[i])
+                {
+                    identical = false;
+                }
+            }
+            return identical;
+        }
+        //public static IList<int> PeopleIndexes(IList<IList<string>> favoriteCompanies)
+        //{
+        //    List<int> res = new List<int>();
+        //    List<HashSet<String>> fcs = new List<HashSet<string>>();            
+        //    foreach(IList<string> fc in favoriteCompanies)
+        //    {
+        //        fcs.Add(new HashSet<string>(fc));
+        //    }
+
+        //    for(int i = 0; i < fcs.Count; i++)
+        //    {
+        //        bool subset = false;
+        //        for (int j = 0; j < fcs.Count; j++)
+        //        {
+        //            if (i != j)
+        //            {
+        //                subset = fcs[i].IsSubsetOf(fcs[j]);
+        //            }
+        //            if (subset)
+        //                break;
+        //        }
+        //        if (!subset)
+        //        {
+        //            res.Add(i);
+        //        }
+        //    }
+
+        //    return res;
+        //}
 
     }
 }
