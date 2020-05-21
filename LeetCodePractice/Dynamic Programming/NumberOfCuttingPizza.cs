cfc07c0af513465442d459fd2d77ba9c7e0c5373
huajian lin <huaj59172507@gmail.com>
2020-05-21 16:33:43 -0400

    cutting pizza problem with naive dp solution

---
+++ LeetCodePractice/Dynamic Programming/NumberOfCuttingPizza.cs
@@ -0,0 +1,100 @@
+﻿using System;
+/*
+Given a rectangular pizza represented as a rows x cols matrix containing the following characters: 'A' (an apple) and '.' (empty cell) and given the integer k. You have to cut the pizza into k pieces using k-1 cuts. 
+
+For each cut you choose the direction: vertical or horizontal, then you choose a cut position at the cell boundary and cut the pizza into two pieces. If you cut the pizza vertically, give the left part of the pizza to a person. If you cut the pizza horizontally, give the upper part of the pizza to a person. Give the last piece of pizza to the last person.
+
+Return the number of ways of cutting the pizza such that each piece contains at least one apple. Since the answer can be a huge number, return this modulo 10^9 + 7.
+
+Example 1:
+
+Input: pizza = ["A..","AAA","..."], k = 3
+Output: 3 
+Explanation: The figure above shows the three ways to cut the pizza. Note that pieces must contain at least one apple.
+Example 2:
+
+Input: pizza = ["A..","AA.","..."], k = 3
+Output: 1
+Example 3:
+
+Input: pizza = ["A..","A..","..."], k = 1
+Output: 1
+ 
+Constraints:
+
+1 <= rows, cols <= 50
+rows == pizza.length
+cols == pizza[i].length
+1 <= k <= 10
+pizza consists of characters 'A' and '.' only.
+
+Sol DP:
+Time O(r^2*c^2*k)
+Space O(r*c*k)
+
+Let sum[i][j] be the number of apples from i to r rows and j to c columns
+Let dp[r][c][k] be the number of way to cut pizza with r rows, c columns, and into k pieces with each pices have at least one apple
+Return dp[0][0][k]
+*/
+namespace LeetCodePractice.Dynamic_Programming
+{
+    class NumberOfCuttingPizza
+    {
+        static void Main()
+        //static void Main1444)
+        {
+            string[] pizza = new string[] { "A..", "AAA", "..." };
+            int k = 3;
+            Console.Out.WriteLine(Ways(pizza,k));
+            Console.In.ReadLine();
+        }
+        public static int Ways(string[] pizza, int k)
+        {
+            const int mod = (int)1e9 + 7;
+            int row = pizza.Length, col = pizza[0].Length;
+            int[,] sum = new int[row+1, col+1];
+            int[,,] dp = new int[row+1, col+1, k+1];
+
+            //calculate sum of apples
+            for (int i = row - 1; i >= 0; --i)
+            {
+                int s = 0;
+                for(int j = col - 1; j >= 0; --j)
+                {
+                    s += pizza[i][j] == 'A' ? 1 : 0;
+                    sum[i, j] = sum[i + 1, j] + s;
+                }
+            }
+
+            for (int i = row - 1; i >= 0; --i)
+            {
+                for (int j = col - 1; j >= 0; --j)
+                {
+                    dp[i, j, 1] = 1;
+                    for(int x = 2; x <= k; ++x)
+                    {
+                        for(int t = i+1; t<row; ++t) 
+                        {
+                            if (sum[t, j] == 0)
+                                break;
+                            if (sum[i, j] > sum[t, j])
+                            {
+                                dp[i, j, x] = (dp[i, j, x] + dp[t, j, x - 1]) % mod;
+                            }                                                                                    
+                        }
+                        for (int t = j+1; t < col; t++)
+                        {
+                            if (sum[i, t] == 0)
+                                break;
+                            if (sum[i, j] > sum[i, t])
+                            {
+                                dp[i, j, x] = (dp[i, j, x] + dp[i, t, x - 1]) % mod;
+                            }
+                        }
+                    }
+                }
+            }
+            return dp[0, 0, k];
+        }
+    }
+}
---
+++ LeetCodePractice/SimpleWarmUp/MaxPowerOfString.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ Given a string s, the power of the string is the maximum length of a non-empty substring that contains only one unique character.
+
+Return the power of the string.
+
+Example 1:
+
+Input: s = "leetcode"
+Output: 2
+Explanation: The substring "ee" is of length 2 with the character 'e' only.
+Example 2:
+
+Input: s = "abbcccddddeeeeedcba"
+Output: 5
+Explanation: The substring "eeeee" is of length 5 with the character 'e' only.
+Example 3:
+
+Input: s = "triplepillooooow"
+Output: 5
+Example 4:
+
+Input: s = "hooraaaaaaaaaaay"
+Output: 11
+Example 5:
+
+Input: s = "tourist"
+Output: 1
+ 
+Constraints:
+
+1 <= s.length <= 500
+s contains only lowercase English letters.
+
+Sol Brute Force:
+Time O(n)
+Space O(1)
+
+Linear scan of string
+ */
+
+namespace LeetCodePractice.SimpleWarmUp
+{
+    class MaxPowerOfString
+    {
+        //static void Main()
+        static void Main1446()
+        {
+            string s = "abbcccddddeeeeedcba";
+            Console.Out.WriteLine(MaxPower(s));
+            Console.In.ReadLine();
+        }
+
+        public static int MaxPower(string s)
+        {
+            int power = 1;
+            int tempCount = 1;
+            char temp = s.First();
+            for(int i = 1; i<s.Length; i++)
+            {
+                char c = s[i];
+                if(temp == c)
+                {
+                    tempCount++;
+                }
+                else
+                {
+                    temp = c;
+                    power = Math.Max(tempCount, power);
+                    tempCount = 1;
+                }
+                
+            }
+            return Math.Max(tempCount, power); 
+        }
+    }
+}
